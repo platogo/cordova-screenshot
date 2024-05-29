5e81d9cb3163a0840af93ffb654b5fd4ed2a5380
Majeed <majeed@platogo.com>
2024-05-29 10:34:37 +0200

    refactor plugin - remove unused if condition to increase android support - clean up unused imports - removed unsed functions
    Co-authored-by: Victoria Kalimullina <victoria@platogo.com>

--- src/android/ScreenshotSaver.java
+++ src/android/ScreenshotSaver.java
@@ -1,16 +1,8 @@
 package com.darktalker.cordova.screenshot;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
 import android.os.Handler;
-import android.provider.MediaStore;
 import android.view.PixelCopy;
 import android.view.View;
 import android.util.Base64;
@@ -23,8 +15,9 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import androidx.core.content.ContextCompat;
+import java.util.Objects;
 
+import androidx.core.content.ContextCompat;
 
 import android.annotation.TargetApi;
 
@@ -35,14 +28,6 @@ public class ScreenshotSaver {
     String fileName;
     CallbackContext pluginContext;
 
-    private void scanPhoto(String imageFileName) {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(imageFileName);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.cordova.getActivity().sendBroadcast(mediaScanIntent);
-    }
-
     public ScreenshotSaver(CordovaInterface cordova, View view, String fileName, CallbackContext pluginContext) {
         this.cordova = cordova;
         this.view = view;
@@ -52,45 +37,28 @@ public class ScreenshotSaver {
 
     public void saveScreenshot(Bitmap bitmap) {
         try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                File storageDir = new File(ContextCompat.getExternalFilesDirs(cordova.getActivity().getApplicationContext(), null)[0] + File.separator + "screenshots", fileName + ".jpg");
 
-                if (!storageDir.getParentFile().exists()) {
-                    storageDir.getParentFile().mkdirs();
-                }
+            File storageDir = new File(
+                    ContextCompat.getExternalFilesDirs(cordova.getActivity().getApplicationContext(), null)[0]
+                            + File.separator + "screenshots",
+                    fileName + ".jpg");
 
-                try {
-                    FileOutputStream fos = new FileOutputStream(storageDir);
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                    JSONObject jsonRes = new JSONObject();
-                    jsonRes.put("filePath", storageDir.getAbsolutePath());
-                    PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
-                    pluginContext.sendPluginResult(result);
-                    fos.close();
-                } catch (Exception e) {
-                    pluginContext.error(e.getMessage());
-                }
+            if (!Objects.requireNonNull(storageDir.getParentFile()).exists()) {
+                storageDir.getParentFile().mkdirs();
+            }
 
-            } else {
-                File folder = new File(Environment.getExternalStorageDirectory(), "Pictures");
-                if (!folder.exists()) {
-                    folder.mkdirs();
-                }
-
-                File f = new File(folder, fileName + ".jpg");
-
-                FileOutputStream fos = new FileOutputStream(f);
-
+            try {
+                FileOutputStream fos = new FileOutputStream(storageDir);
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-
                 JSONObject jsonRes = new JSONObject();
-                jsonRes.put("filePath", f.getAbsolutePath());
+                jsonRes.put("filePath", storageDir.getAbsolutePath());
                 PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
                 pluginContext.sendPluginResult(result);
-
-                scanPhoto(f.getAbsolutePath());
                 fos.close();
+            } catch (Exception e) {
+                pluginContext.error(e.getMessage());
             }
+
         } catch (Exception e) {
             pluginContext.error(e.getMessage());
 
@@ -110,13 +78,7 @@ public class ScreenshotSaver {
                 jsonRes.put("URI", js_out);
                 PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
                 pluginContext.sendPluginResult(result);
-
-                js_out = null;
-                output = null;
-                code = null;
             }
-
-            jpeg_data = null;
 
         } catch (Exception e) {
             pluginContext.error(e.getMessage());
@@ -153,7 +115,7 @@ public class ScreenshotSaver {
             PixelCopy.request(cordova.getActivity().getWindow(), rect, bitmap, listener, new Handler());
         } catch (IllegalArgumentException e) {
 
-            e.printStackTrace();
+            pluginContext.error("PixelCopy resulted with error: " + e.getMessage());
         }
     }
 
